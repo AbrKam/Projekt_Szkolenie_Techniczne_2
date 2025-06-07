48348265264b3f4c8b4651e883d4cee4db7e1b09
Kamil Abram <kamilabram67@gmail.com>
2025-06-07 19:24:37 +0200

    frontend init

---
+++ frontend/vetclinic-client/src/components/Header.tsx
@@ -0,0 +1,18 @@
+function Header()
+{
+    return(
+        <header>
+            <h1>VetClinicApp</h1>
+            <nav>
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
